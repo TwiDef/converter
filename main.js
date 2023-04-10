8c70508cf575d2c3332ebc688400062b9b703a39
Def <deferenseo@gmail.com>
2023-04-10 20:53:42 +0300

    fetch courses (usd,eur,gbp)

---
+++ main.js
@@ -0,0 +1,29 @@
+'use strict';
+
+const url = 'https://www.cbr-xml-daily.ru/daily_json.js';
+
+const elementUSD = document.querySelector('[data-value="USD"]');
+const elementEUR = document.querySelector('[data-value="EUR"]');
+const elementGBP = document.querySelector('[data-value="GBP"]');
+
+const rates = {};
+
+getCurrencies();
+
+async function getCurrencies() {
+    const response = await fetch(url);
+    const data = await response.json();
+    const result = await data;
+
+    rates.USD = result.Valute.USD;
+    rates.EUR = result.Valute.EUR;
+    rates.GBP = result.Valute.GBP;
+
+    elementUSD.textContent = rates.USD.Value.toFixed(2);
+    elementEUR.textContent = rates.EUR.Value.toFixed(2);
+    elementGBP.textContent = rates.GBP.Value.toFixed(2);
+
+
+}
+
+console.log(rates);
